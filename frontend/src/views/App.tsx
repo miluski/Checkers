4ed5800187d152cf3ea0de5f8a05f2d4d508d785
sprytnykowal@gmail.com <sprytnykowal@gmail.com>
2024-04-03 21:44:58 +0200

    Created mainpage

--- frontend/src/views/App.tsx
+++ frontend/src/views/App.tsx
@@ -1,25 +1,25 @@
-import { useState } from 'react'
-import './App.css'
+import { Button } from "react-bootstrap";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<div className='backgroundDiv d-flex flex-column justify-content-center align-items-center position-absolute w-100 h-100'>
+			<div className='centeredDiv d-flex flex-column bg-light w-50 h-75 rounded-5 mb-5'>
+				<div className='d-flex flex-column justify-content-center align-items-center w-100 m-3'>
+					<text className='checkersText fs-1 fw-bold'>Warcaby</text>
+				</div>
+				<div className='d-flex flex-column justify-content-center align-items-center w-100'>
+					<Button className='btn btn-primary fs-4 fw-light w-75 m-4'>Zaloguj/Zarejestruj</Button>
+          <Button className='btn btn-primary fs-4 fw-light w-75 m-4'>SinglePlayer</Button>
+          <Button className='btn btn-primary fs-4 fw-light w-75 m-4'>Multiplayer</Button>
+          <Button className='btn btn-primary fs-4 fw-light w-75 m-4'>Wczytaj grę</Button>
+				</div>
+			</div>
+      <div className='d-flex flex-column justify-content-center align-items-center w-100 mt-5'>
+          <text className='copyright fs-4'>@Copyright: Maksymilian Sowula, Jakub Szczur, Filip Skibiński, Karol Przygoda</text>
+        </div>
+		</div>
+	);
 }
 
-export default App
+export default App;
