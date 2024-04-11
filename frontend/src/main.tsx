61e83f72bbae7c639455958f24c01d3adef7792b
JacobJS7 <j.szczur@interia.pl>
2024-04-11 16:08:25 +0200

    Added Screen after login view and assets

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './views/App.tsx'
+import Screen_after_login from './views/Screen_after_login.tsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Screen_after_login />
   </React.StrictMode>,
 )
